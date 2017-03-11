fa09d9facad0edb25e5beeea57f1dcd29b5a6e37
Fy Project <fyproject2k16@gmail.com>
2017-03-11 15:45:22 +0530

    complaint app added

--- app/src/main/java/com/fyproject/shrey/ewrittenappclient/fragments/AppTypeFragments/ComplaintFragment.java
+++ app/src/main/java/com/fyproject/shrey/ewrittenappclient/fragments/AppTypeFragments/ComplaintFragment.java
@@ -6,10 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.fyproject.shrey.ewrittenappclient.R;
 
 public class ComplaintFragment extends Fragment {
+
+
+    Spinner spSelectFaculty;
+    Spinner spComplaintType;
+    RadioGroup rgLevel;
+    TextView tvDescription;
+    TextView tvShowFileName;
+    ImageView ivFileUpload;
+    Button btnSubmit;
+
 
 
     public ComplaintFragment() {
@@ -20,8 +36,36 @@ public class ComplaintFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wapptype_leave, container, false);
-
+            initialization(view);
         return view;
     }
 
+    private void initialization(View view) {
+        spSelectFaculty = (Spinner) view.findViewById(R.id.spSelectFaculty);
+        spComplaintType = (Spinner) view.findViewById(R.id.spComplaintType);
+        rgLevel = (RadioGroup) view.findViewById(R.id.rgLevel);
+        tvDescription = (TextView) view.findViewById(R.id.tvDescription);
+        tvShowFileName = (TextView) view.findViewById(R.id.tvShowFileName);
+        ivFileUpload = (ImageView) view.findViewById(R.id.ivFileUpload);
+        btnSubmit = (Button) view.findViewById(R.id.btnSubmit);
+    }
+
+    //call these method according to your requirements
+    private void fileUploadChooser(){
+        ivFileUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //choose file and show the file name into the tvShowFileName
+            }
+        });
+    }
+
+    private void btnSubmitClickEvent(){
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //upload data to firebase
+            }
+        });
+    }
 }
